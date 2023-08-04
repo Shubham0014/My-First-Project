c0e3de1fe7c592649caf2a508abf6f975d909f14
Shubham0014 <shubham7744088156@gmail.com>
2023-08-04 23:59:59 +0530

    first commit

---
+++ src/main/java/Resources/cons.java
@@ -0,0 +1,12 @@
+package Resources;
+
+public class cons {
+
+	public static String ID="shubham7744088156@gmail.com";
+	public static String Pass="shubham7744";
+
+	//search for iphone
+	public static String prod="iphone";
+	//search for samsung tab
+	public static String prod2="Samsung";
+}
